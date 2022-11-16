dde8bf04f75539988c303de761081dc7eb0aca15
Reja <rezanurrochmat@gmail.com>
2022-11-16 21:18:48 +0700

    add get by id, update and delete post

--- router/post.router.js
+++ router/post.router.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const formidableMiddleware = require('formidable');
+const { where } = require('sequelize');
 const model = require('../models/index.js');
 const Post = model.post;
+
+// All
+router.get('/api/posts', (request, response) => {
+    Post.findAll().then(result => {
+        response.status(200).json({data: result});
+    }).catch(err => {
+        console.error(err);
+        throw err;
+    });
+});
+
+// Get By ID
+router.get('/api/posts/:id', (request, response) => {
+    Post.findByPk(request.params.id).then(result => {
+        if(result == null) {
+            response.status(404).json({data: {}});
+            return;
+        }
+        response.status(200).json({data: result});
+    }).catch(err => {
+        console.error(err);
+        throw err;
+    });
+});
 
 // Create
 router.post('/api/posts', (request, response) => {
@@ -12,16 +37,65 @@ router.post('/api/posts', (request, response) => {
         if (err) {
             next(err);
             return;
-        }
+        };
 
         Post.create(fields).then(result => {
             response.status(201).json({
                 message: "Created post successfully",
                 payload: fields
             });
-        })
+        });
 
-    })
+    });
+});
+
+// Update
+router.put('/api/posts/:id', (request, response) => {
+    const form = formidableMiddleware({ });
+
+    form.parse(request, (err, fields, files) => {
+        if (err) {
+            next(err);
+            return;
+        };
+
+        Post.findByPk(request.params.id).then(result => {
+            if(result == null) {
+                response.status(404).json({data: {}});
+                return;
+            }
+
+            Post.update(fields, {where: {id: request.params.id}}).then(result => {
+                response.status(200).json({data: result});
+            }).catch(err => {
+                console.error(err);
+                throw err;
+            });
+            
+        }).catch(err => {
+            console.error(err);
+            throw err;
+        });
+
+    });
+});
+
+// Delete
+router.delete('/api/posts/:id', (request, response) => {
+    Post.findByPk(request.params.id).then(result => {
+        if(result == null) {
+            response.status(404).json({data: {}});
+            return;
+        }
+
+        Post.destroy({where: {id: request.params.id}}).then(result => {
+            response.status(204).json({data: {}});
+        });
+        
+    }).catch(err => {
+        console.error(err);
+        throw err;
+    });
 });
 
 module.exports = router;
