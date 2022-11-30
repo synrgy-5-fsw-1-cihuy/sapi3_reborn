fd1f14b7e25de40c36e48d8fcf165ee6d93b2e2c
Ryan Jonathan <ryanjnthan@gmail.com>
2022-11-30 21:17:55 +0700

    init

--- router/post.router.js
+++ router/post.router.js
@@ -1,18 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const formidableMiddleware = require('formidable');
-const authMiddlware = require('../middleware/auth_token.js');
-const models = require('../models');
+const formidableMiddleware = require("formidable");
+const authMiddlware = require("../middleware/auth_token.js");
+const models = require("../models");
 const Post = models.Post;
 
 // router.use(authMiddlware);
-
 
 /**
  * @swagger
  *  components:
  *   schemas:
- *     Post: 
+ *     Post:
  *      type: object
  *      required:
  *        - id
@@ -49,12 +48,14 @@ const Post = models.Post;
  */
 
 // All
-router.get('/api/posts', (request, response) => {
-    Post.findAll().then(result => {
-        response.status(200).json({data: result});
-    }).catch(err => {
-        console.error(err);
-        throw err;
+router.get("/api/posts", (request, response) => {
+  Post.findAll()
+    .then((result) => {
+      response.status(200).json({ data: result });
+    })
+    .catch((err) => {
+      console.error(err);
+      throw err;
     });
 });
 
@@ -77,85 +78,107 @@ router.get('/api/posts', (request, response) => {
  */
 
 // Get By ID
-router.get('/api/posts/:id', (request, response) => {
-    Post.findByPk(request.params.id).then(result => {
-        if(result == null) {
-            response.status(404).json({data: {}});
-            return;
-        }
-        response.status(200).json({data: result});
-    }).catch(err => {
-        console.error(err);
-        throw err;
+router.get("/api/posts/:id", (request, response) => {
+  Post.findByPk(request.params.id)
+    .then((result) => {
+      if (result == null) {
+        response.status(404).json({ data: {} });
+        return;
+      }
+      response.status(200).json({ data: result });
+    })
+    .catch((err) => {
+      console.error(err);
+      throw err;
     });
 });
 
+/**
+ * @swagger
+ *   /posts/{id}:
+ *      post:
+ *        summary: Create a new post (file)
+ *        tags: [Posts]
+ *        parameters:
+ *          - in: path
+ *            name: id
+ *        responses:
+ *          "200":
+ *            description: Retrieve posts by id
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  $ref: '#/components/schemas/Post'
+ */
+
 // Create
-router.post('/api/posts', (request, response) => {
-    const form = formidableMiddleware({ });
+router.post("/api/posts", (request, response) => {
+  const form = formidableMiddleware({});
 
-    form.parse(request, (err, fields, files) => {
-        if (err) {
-            next(err);
-            return;
-        };
+  form.parse(request, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
 
-        Post.create(fields).then(result => {
-            response.status(201).json({
-                message: "Created post successfully",
-                payload: fields
-            });
-        });
-
+    Post.create(fields).then((result) => {
+      response.status(201).json({
+        message: "Created post successfully",
+        payload: fields,
+      });
     });
+  });
 });
 
 // Update
-router.put('/api/posts/:id', (request, response) => {
-    const form = formidableMiddleware({ });
+router.put("/api/posts/:id", (request, response) => {
+  const form = formidableMiddleware({});
 
-    form.parse(request, (err, fields, files) => {
-        if (err) {
-            next(err);
-            return;
-        };
+  form.parse(request, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
 
-        Post.findByPk(request.params.id).then(result => {
-            if(result == null) {
-                response.status(404).json({data: {}});
-                return;
-            }
+    Post.findByPk(request.params.id)
+      .then((result) => {
+        if (result == null) {
+          response.status(404).json({ data: {} });
+          return;
+        }
 
-            Post.update(fields, {where: {id: request.params.id}}).then(result => {
-                response.status(200).json({data: result});
-            }).catch(err => {
-                console.error(err);
-                throw err;
-            });
-            
-        }).catch(err => {
+        Post.update(fields, { where: { id: request.params.id } })
+          .then((result) => {
+            response.status(200).json({ data: result });
+          })
+          .catch((err) => {
             console.error(err);
             throw err;
-        });
-
-    });
+          });
+      })
+      .catch((err) => {
+        console.error(err);
+        throw err;
+      });
+  });
 });
 
 // Delete
-router.delete('/api/posts/:id', (request, response) => {
-    Post.findByPk(request.params.id).then(result => {
-        if(result == null) {
-            response.status(404).json({data: {}});
-            return;
-        }
+router.delete("/api/posts/:id", (request, response) => {
+  Post.findByPk(request.params.id)
+    .then((result) => {
+      if (result == null) {
+        response.status(404).json({ data: {} });
+        return;
+      }
 
-        Post.destroy({where: {id: request.params.id}}).then(result => {
-            response.status(204).json({data: {}});
-        });
-        
-    }).catch(err => {
-        console.error(err);
-        throw err;
+      Post.destroy({ where: { id: request.params.id } }).then((result) => {
+        response.status(204).json({ data: {} });
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      throw err;
     });
 });
 
