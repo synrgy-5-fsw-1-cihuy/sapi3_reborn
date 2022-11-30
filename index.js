b556b1236e8ada788236f2994bb2667d219382c0
Khoironi Kurnia Syah <zekhoi.app@gmail.com>
2022-11-30 22:29:05 +0800

    haduh user aja  deh

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-const express = require('express');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const postRouter = require('./router/post.router.js');
-const carRouter = require('./router/car.router.js');
-const userRouter = require('./router/user.router.js');
+const express = require("express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const postRouter = require("./router/post.router.js");
+const carRouter = require("./router/car.router.js");
+const userRouter = require("./router/user.router.js");
 
 const PORT = 8001 || process.env.PORT;
 const app = express();
 
 // Set view engine EJS
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 const options = {
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "Binar Synrgy Express API with Swagger",
-        version: "0.1.0",
-        description:
-          "This is a simple CRUD API application made with Express and documented with Swagger",
-        license: {
-          name: "MIT",
-          url: "https://spdx.org/licenses/MIT.html",
-        },
-        contact: {
-          name: "Binar Synrgy",
-          url: "https://binaracademy.com",
-          email: "binaracademy.com",
-        },
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Binar Synrgy Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
       },
-      servers: [
-        {
-          url: "http://localhost:8001/api/",
-        },
-      ],
+      contact: {
+        name: "Binar Synrgy",
+        url: "https://binaracademy.com",
+        email: "binaracademy.com",
+      },
     },
-    apis: [
-        "./router/post.router.js"
+    servers: [
+      {
+        url: "http://localhost:8001/api/",
+      },
     ],
+  },
+  apis: ["./router/post.router.js", "./router/user.router.js"],
 };
 const specs = swaggerJsDoc(options);
 
@@ -48,5 +46,5 @@ app.use(carRouter);
 app.use(userRouter);
 
 app.listen(PORT, () => {
-    console.log(`Application running at localhost:${PORT}`);
+  console.log(`Application running at localhost:${PORT}`);
 });
--- router/user.router.js
+++ router/user.router.js
@@ -1,9 +1,60 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controller/user.controller.js');
+const userController = require("../controller/user.controller.js");
 
-router.post('/api/auth/register', userController.registerUser);
-router.post('/api/auth/login', userController.loginHandler);
+/**
+ * @swagger
+ *  components:
+ *   schemas:
+ *     User:
+ *      type: object
+ *      required:
+ *        - id
+ *        - email
+ *        - password
+ *      properties:
+ *        id:
+ *          type: integer
+ *          description: The generated ID's Post model
+ *        email:
+ *          type: string
+ *          description: The email your post
+ *        password:
+ *          type: string
+ *          description: The password your post
+ */
 
+/**
+ * @swagger
+ *   /api/auth/register:
+ *      post:
+ *        summary: Create a new user
+ *        tags: [Users]
+ *        responses:
+ *          "201":
+ *            description: Retrieve all posts
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  $ref: '#/components/schemas/User'
+ */
+
+router.post("/api/auth/register", userController.registerUser);
+
+/**
+ * @swagger
+ *   /api/auth/login:
+ *      post:
+ *        summary: Login a user
+ *        tags: [Users]
+ *        responses:
+ *          "200":
+ *            description: Retrieve all posts
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  $ref: '#/components/schemas/User'
+ */
+router.post("/api/auth/login", userController.loginHandler);
 
 module.exports = router;
